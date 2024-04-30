f685106b7e69e88b755350dc46273a824acacd02
Henry Bui <henry.h.bui05@gmail.com>
2024-04-29 20:19:43 -0400

    add skeleton impl for ensemble_test.core

--- python-src/ensemble_test/ensemble_test.pyi
+++ python-src/ensemble_test/ensemble_test.pyi
@@ -10,3 +10,125 @@
 
 class Simulator:
     def __new__(cls): pass
+
+    # Loading simulator code
+    def load_file(self, src_fp: str) -> None: pass
+    def load_code(self, src: str) -> None: pass
+
+    # Simulation
+    def run(self, limit: int | None) -> None: pass
+    def step_in(self) -> None: pass
+    def step_out(self) -> None: pass
+    def step_over(self) -> None: pass
+
+    # Memory access
+    def read_mem(self, addr: int) -> int: pass
+    def write_mem(self, addr: int, val: int) -> None: pass
+    def get_mem(self, addr: int) -> int: pass
+    def set_mem(self, addr: int, val: int) -> None: pass
+
+    # Register access
+    @property
+    def r0(self) -> int: pass
+    @r0.setter
+    def r0(self, value: int): pass
+    @property
+    def r1(self) -> int: pass
+    @r1.setter
+    def r1(self, value: int): pass
+    @property
+    def r2(self) -> int: pass
+    @r2.setter
+    def r2(self, value: int): pass
+    @property
+    def r3(self) -> int: pass
+    @r3.setter
+    def r3(self, value: int): pass
+    @property
+    def r4(self) -> int: pass
+    @r4.setter
+    def r4(self, value: int): pass
+    @property
+    def r5(self) -> int: pass
+    @r5.setter
+    def r5(self, value: int): pass
+    @property
+    def r6(self) -> int: pass
+    @r6.setter
+    def r6(self, value: int): pass
+    @property
+    def r7(self) -> int: pass
+    @r7.setter
+    def r7(self, value: int): pass
+    def get_reg(self, index: int) -> int: pass
+    def set_reg(self, index: int, val: int) -> None: pass
+
+    # Label lookup
+    def lookup(self, label: str) -> int | None: pass
+    def reverse_lookup(self, addr: int) -> str | None: pass
+    def add_label(self, label: str, addr: int) -> bool: pass
+    def delete_label(self, label: str) -> None: pass
+
+    # Breakpoints
+    def add_breakpoint(self, break_loc: int | str): pass
+    def remove_breakpoint(self, break_loc: int | str): pass
+    
+    @property
+    def breakpoints(self) -> dict[int, None]: pass
+    # Miscellaneous access
+    @property
+    def n(self) -> bool: pass
+    @property
+    def z(self) -> bool: pass
+    @property
+    def p(self) -> bool: pass
+
+    @property
+    def pc(self) -> int: pass
+    @pc.setter
+    def pc(self, addr: int) -> None: pass
+
+    @property
+    def executions(self) -> int: pass
+
+    # Configuration settings
+    @property
+    def use_real_halt(self) -> bool: pass
+    @use_real_halt.setter
+    def use_real_halt(self, status: bool) -> None: pass
+    
+    @property
+    def strict_execution(self) -> bool: pass
+    @strict_execution.setter
+    def strict_execution(self, status: bool) -> None: pass
+    
+    # I/O
+    @property
+    def input(self) -> str: pass
+    @input.setter
+    def input(self, input: str) -> None: pass
+    
+    @property
+    def output(self) -> str: pass
+    @output.setter
+    def output(self, output: str) -> None: pass
+    
+    # from pylc3
+    # TODO: determine if these are  necessary
+    # @property
+    # def max_call_stack_size(self) -> int: pass
+    # @max_call_stack_size.setter
+    # def max_call_stack_size(self, size: int) -> None: pass
+
+    # def disassemble(self, addr: int, level: int) -> str: pass
+    # def disassemble_data(self, addr: int, level: int) -> str: pass
+    
+    # def add_subroutine_info(self, subroutine_label: str, n_params: int) -> bool: pass
+    
+    # def first_level_calls(self) -> list[None]: pass
+    # def first_level_traps(self) -> list[None]: pass
+
+class LoadError(ValueError):
+    pass
+class SimError(ValueError):
+    pass
--- src/lib.rs
+++ src/lib.rs
@@ -35,6 +35,11 @@ impl LoadError {
     }
 }
 
+#[derive(FromPyObject)]
+enum BreakpointLocation {
+    Address(u16),
+    Label(String)
+}
 #[pyclass(name="Simulator")]
 struct PySimulator {
     sim: Simulator,
@@ -47,39 +52,230 @@ impl PySimulator {
         PySimulator { sim: Simulator::new(Default::default()) }
     }
 
-    fn init(&mut self, src_fp: &str) -> PyResult<()> {
-        self.sim = Simulator::new(Default::default());
+    fn load_file(&mut self, src_fp: &str) -> PyResult<()> {
+        todo!()
+    }
+    fn load_code(&mut self, src: &str) -> PyResult<()> {
+        todo!()
+    }
+
+    fn run(&mut self, limit: Option<u64>) -> PyResult<()> {
+        todo!()
+    }
+    fn step_in(&mut self) -> PyResult<()> {
+        todo!()
+    }
+    fn step_out(&mut self) -> PyResult<()> {
+        todo!()
+    }
+    fn step_over(&mut self) -> PyResult<()> {
+        todo!()
+    }
+
+    fn read_mem(&mut self, addr: u16) -> PyResult<u16> {
+        todo!()
+    }
+    fn write_mem(&mut self, addr: u16, val: u16) -> PyResult<()> {
+        todo!()
+    }
+    fn get_mem(&self, addr: u16) -> u16 {
+        todo!()
+    }
+    fn set_mem(&mut self, addr: u16, val: u16) {
+        todo!()
+    }
+
+    #[getter]
+    fn get_r0(&self) -> u16 {
+        todo!()
+    }
+    #[setter]
+    fn set_r0(&mut self, value: u16) {
+        todo!()
+    }
+    #[getter]
+    fn get_r1(&self) -> u16 {
+        todo!()
+    }
+    #[setter]
+    fn set_r1(&mut self, value: u16) {
+        todo!()
+    }
+    #[getter]
+    fn get_r2(&self) -> u16 {
+        todo!()
+    }
+    #[setter]
+    fn set_r2(&mut self, value: u16) {
+        todo!()
+    }
+    #[getter]
+    fn get_r3(&self) -> u16 {
+        todo!()
+    }
+    #[setter]
+    fn set_r3(&mut self, value: u16) {
+        todo!()
+    }
+    #[getter]
+    fn get_r4(&self) -> u16 {
+        todo!()
+    }
+    #[setter]
+    fn set_r4(&mut self, value: u16) {
+        todo!()
+    }
+    #[getter]
+    fn get_r5(&self) -> u16 {
+        todo!()
+    }
+    #[setter]
+    fn set_r5(&mut self, value: u16) {
+        todo!()
+    }
+    #[getter]
+    fn get_r6(&self) -> u16 {
+        todo!()
+    }
+    #[setter]
+    fn set_r6(&mut self, value: u16) {
+        todo!()
+    }
+    #[getter]
+    fn get_r7(&self) -> u16 {
+        todo!()
+    }
+    #[setter]
+    fn set_r7(&mut self, value: u16) {
+        todo!()
+    }
+    fn get_reg(&self, index: usize) -> PyResult<u16> {
+        todo!()
+    }
+    fn set_reg(&mut self, index: usize, val: u16) -> PyResult<()> {
+        todo!()
+    }
+
+    fn lookup(&self, label: &str) -> Option<u16> {
+        todo!()
+    }
+    fn reverse_lookup(&self, addr: u16) -> Option<&str> {
+        todo!()
+    }
+    fn add_label(&mut self, label: &str, addr: u16) -> bool {
+        todo!()
+    }
+    fn delete_label(&mut self, label: &str) {
+        todo!()
+    }
+
+    fn add_breakpoint(&mut self, break_loc: BreakpointLocation) {
+        todo!()
+    }
+    fn remove_breakpoint(&mut self, break_loc: BreakpointLocation) {
+        todo!()
+    }
+    
+    fn breakpoints(&self) -> Vec<u16> {
+        todo!()
+    }
+
+    #[getter]
+    fn get_n(&self) -> bool {
+        todo!()
+    }
+    #[getter]
+    fn get_z(&self) -> bool {
+        todo!()
+    }
+    #[getter]
+    fn get_p(&self) -> bool {
+        todo!()
+    }
+
+    #[getter]
+    fn get_pc(&self) -> u16 {
+        todo!()
+    }
+    #[setter]
+    fn set_pc(&mut self, addr: u16) {
+        todo!()
+    }
+
+    #[getter]
+    fn get_executions(&self) -> u64 {
+        todo!()
+    }
+
+    #[getter]
+    fn get_use_real_halt(&self) -> bool {
+        todo!()
+    }
+    #[setter]
+    fn set_use_real_halt(&mut self, status: bool) {
+        todo!()
+    }
+    
+    #[getter]
+    fn get_strict_execution(&self) -> bool {
+        todo!()
+    }
+    #[setter]
+    fn set_strict_execution(&mut self, status: bool) {
+        todo!()
+    }
+    
+    #[getter]
+    fn get_input(&self) -> &str {
+        todo!()
+    }
+    #[setter]
+    fn set_input(&mut self, input: &str) {
+        todo!()
+    }
+    
+    #[getter]
+    fn get_output(&self) -> &str {
+        todo!()
+    }
+    #[setter]
+    fn set_output(&mut self, output: &str) {
+        todo!()
+    }
+
+    // fn init(&mut self, src_fp: &str) -> PyResult<()> {
+    //     self.sim = Simulator::new(Default::default());
         
-        let src = std::fs::read_to_string(src_fp)?;
-        let ast = parse_ast(&src)
-            .map_err(LoadError::from_lc3_err)?;
-        let obj = assemble_debug(ast, &src)
-            .map_err(LoadError::from_lc3_err)?;
+    //     let src = std::fs::read_to_string(src_fp)?;
+    //     let ast = parse_ast(&src)
+    //         .map_err(LoadError::from_lc3_err)?;
+    //     let obj = assemble_debug(ast, &src)
+    //         .map_err(LoadError::from_lc3_err)?;
 
-        self.sim.load_obj_file(&obj);
-        Ok(())
-    }
+    //     self.sim.load_obj_file(&obj);
+    //     Ok(())
+    // }
 
-    fn run(&mut self) -> PyResult<()> {
-        self.sim.run()
-            .map_err(|e| SimError::new_err(format!("{e} (PC: 0x{})", self.sim.prefetch_pc())))
-    }
+    // fn run(&mut self) -> PyResult<()> {
+    //     self.sim.run()
+    //         .map_err(|e| SimError::new_err(format!("{e} (PC: 0x{})", self.sim.prefetch_pc())))
+    // }
 
-    fn get_reg(&self, reg: i32) -> PyResult<u16> {
-        match reg {
-            0 => Ok(self.sim.reg_file[R0].get()),
-            1 => Ok(self.sim.reg_file[R1].get()),
-            2 => Ok(self.sim.reg_file[R2].get()),
-            3 => Ok(self.sim.reg_file[R3].get()),
-            4 => Ok(self.sim.reg_file[R4].get()),
-            5 => Ok(self.sim.reg_file[R5].get()),
-            6 => Ok(self.sim.reg_file[R6].get()),
-            7 => Ok(self.sim.reg_file[R7].get()),
-            _ => Err(PyErr::new::<PyIndexError, _>("Invalid Register Specified"))
-        }
-    }
+    // fn get_reg(&self, reg: i32) -> PyResult<u16> {
+    //     match reg {
+    //         0 => Ok(self.sim.reg_file[R0].get()),
+    //         1 => Ok(self.sim.reg_file[R1].get()),
+    //         2 => Ok(self.sim.reg_file[R2].get()),
+    //         3 => Ok(self.sim.reg_file[R3].get()),
+    //         4 => Ok(self.sim.reg_file[R4].get()),
+    //         5 => Ok(self.sim.reg_file[R5].get()),
+    //         6 => Ok(self.sim.reg_file[R6].get()),
+    //         7 => Ok(self.sim.reg_file[R7].get()),
+    //         _ => Err(PyErr::new::<PyIndexError, _>("Invalid Register Specified"))
+    //     }
+    // }
 
-    fn get_memory(&mut self, address: u16) -> PyResult<u16>{
-        Ok(self.sim.mem.get_raw(address).get())
-    }
+    // fn get_memory(&mut self, address: u16) -> PyResult<u16>{
+    //     Ok(self.sim.mem.get_raw(address).get())
+    // }
 }
